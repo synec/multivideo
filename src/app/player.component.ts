44790e2ba10e1993941fb0df5781438312d42ed3
DanielG <synec@users.noreply.github.com>
2022-09-08 22:24:12 +0200

    ng add @angular-eslint/schematics

--- src/app/player.component.ts
+++ src/app/player.component.ts
@@ -1,18 +1,24 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'mvi-player',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+  ],
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.scss']
 })
 export class PlayerComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private ar: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    console.log('this.ar', this.ar);
   }
 
 }
