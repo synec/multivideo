e2e2558beb7c02a039b68ed0443c45fc713ae0aa
DanielG <synec@users.noreply.github.com>
2022-09-08 21:37:31 +0200

    Bootstrap PlayerComponent

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
 
-import { AppModule } from './app/app.module';
+import { PlayerComponent } from './app/player.component';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(PlayerComponent)
   .catch(err => console.error(err));
